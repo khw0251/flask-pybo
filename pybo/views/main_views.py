5b359f130ec2d3b922c4fb7b2007f5a4f03a18d3
김현우 <khw0251@naver.com>
2020-12-19 02:59:11 +0900

    최초 커밋

---
+++ pybo/views/main_views.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, render_template, request
+import sklearn
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
+import keras.backend as K
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+def make_dataset(data, window_size=5):  # 수정
+    feature_list = []
+
+    for i in range(len(data) - window_size + 1):
+        feature_list.append(np.array(data.iloc[i:i + window_size]))
+
+    return np.array(feature_list)
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method == 'GET':
+        return render_template('detection/detection.html')
+
+    if request.method == 'POST':
+        f = (request.files['file'])
+
+        from keras.models import model_from_json
+        json_file = open("real64model.json", "r")
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights("real64model_weight.h5")
+
+
+        s = pd.read_csv(f, header=None)
+        s = s.astype(float) / 255
+        s = s.dropna(axis=1)  # 데이터에서 NaN이 존재한다면 해당 열을 제거
+        s_ = make_dataset(s, 5)
+        predictions = loaded_model.predict(s_)
+        c = 0
+
+        for j in range(0, s_.shape[0]):
+            c += mean_squared_error(s_[j, 0, :], predictions[j, 0, :])
+
+        c = c / s_.shape[0]
+        pro = c
+        return render_template('detection/detection.html', pro=pro)
